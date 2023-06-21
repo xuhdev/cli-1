12c9068bba1d25529ca1eeb320a60e9d57978791
Luke Karrys <luke@lukekarrys.com>
2023-06-21 14:11:09 -0700

    chore: normalize line endings and symlinks
    This makes it possible to do a fresh clone of this repo in Windows, and
    be able to run the resetdeps command successfully and then have a clean
    git status. We now check for a clean git status in during CI in Windows
    which was previously excluded.
    
    The changes required were to:
    - Delete our workspace symlinks from node_modules and add a step to
      resetdeps that will do a platform dependent symlink before the initial
      reification
    - Add line ending normalization to our `.gitattributes` file and commit
      the result. We now normalize everything to `lf` in git and force `bat`
      and `cmd` files to use `crlf` upon checkout.
    - Update workflows to call `node scripts/resetdeps.js` directly since a
      fresh clone of the repo wont have any symlinks in place, so any
      `node .` command will fail due to missing workspaces.
    
    Ref: npm/statusboard#562

--- scripts/resetdeps.js
+++ scripts/resetdeps.js
@@ -1,9 +1,18 @@
-
 const { join } = require('path')
+const { symlink } = require('fs/promises')
 const { CWD, run, pkg, fs, git, npm } = require('./util.js')
 const ciInfo = require('ci-info')
 
-const checkout = () => git('checkout', 'node_modules/')
+const cleanup = async () => {
+  await git('checkout', 'node_modules/')
+  for (const { name, path, pkg: wsPkg } of await pkg.mapWorkspaces()) {
+    if (!wsPkg.private) {
+      // add symlinks similar to how arborist does for our production
+      // workspaces, so they are in place before the initial install.
+      await symlink(path, join(CWD, 'node_modules', name), 'junction')
+    }
+  }
+}
 
 const main = async ({ packageLock }) => {
   await fs.rimraf(join(CWD, 'node_modules'))
@@ -11,7 +20,7 @@ const main = async ({ packageLock }) => {
     await fs.rimraf(join(path, 'node_modules'))
   }
 
-  await checkout()
+  await cleanup()
   await npm('i', '--ignore-scripts', '--no-audit', '--no-fund', packageLock && '--package-lock')
   await npm('rebuild', '--ignore-scripts')
   if (!ciInfo.isCI) {
@@ -19,4 +28,4 @@ const main = async ({ packageLock }) => {
   }
 }
 
-run(main).catch(checkout)
+run(main).catch(cleanup)
